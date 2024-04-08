394e11aeeb1c237aa8bc4297e689fa227b18a493
ALUMNO - LUIS RAUL ZAMBRANO LEON <u23211674@utp.edu.pe>
2024-04-08 02:53:32 -0500

    configuracion de los estilo, vistas hotel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\HotelController;
 use Illuminate\Support\Facades\Route;
 
+
+// Ruta para la página de inicio o vista principal
 Route::get('/', function () {
-    return view('welcome');
+    return view('pruebas.cuadros');
+});
+
+Route::controller(HotelController::class)->group(function(){
+    Route::get('hotel', 'index');
 });
